9e6cd875e6a17d8f463433b3df1206d4d7087892
9hun-se <kevin990114@gmail.com>
2025-01-10 16:28:17 +0900

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,13 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <algorithm>
 
+void levelUp() {
+    level++;
+    maxHealth += level + (maxHealth / 5);
+    currentHealth = maxHealth;
+    damage += level + (damage / 10);
+    exp = 0;
+    std::cout << "레벨업! 현재 레벨: " << level << ", 체력: " << currentHealth
+        << ", 데미지: " << Attack << "\n";
+}
