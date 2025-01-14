9ea116559dba779fc1431b44e331195b970a13d6
9hun-se <kevin990114@gmail.com>
2025-01-14 16:49:23 +0900

    commit_1

--- Player.h
+++ Player.h
@@ -44,6 +44,23 @@ public:
     void recoverHealth();                                     
     int getMaxHealth() const;    //    ***** 추가 *****   Skillinterfaces 참조
     void Heal(int amount);
+
+    //경험치 획득과 레벨업 관련 함수
+    int getLevel()const;
+    void levelUp();
+    void gainExp(int amount);
+
+    //데미지와 관련된 함수
+    void takeDamage(int amount);
+
+    //몬스터 공격시 출력
+    void attackMonster();
+
+    //getter setter 추가
+    std::string getName() const;
+    int getCurrHP()const;
+    void setCurrHP(int newHP);
+
 };
 
 #endif
