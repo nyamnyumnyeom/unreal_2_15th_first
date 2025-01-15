9de2f635370679f7c229d4f62657b1bc853fdd07
nyamnyumnyeom <jincar000@naver.com>
2025-01-15 15:42:56 +0900

    Update V. 0.1
    Delay Cls 추가

--- Battle.cpp
+++ Battle.cpp
@@ -38,8 +38,8 @@ void Battle::nextStage() {
 }
 
 //플레이어와 몬스터의 스텟을 출력하는 함수
-void Battle::showStats()
-{
+void Battle::showStats() {
+	system("cls"); // 화면 지우기
 	cout << setw(20) << left << "┌------------- Level " << setw(8) << right << nowPlayer->getLevel() << setw(15) << right << " -------------┐" << "          " << setw(20) << left << "┌------------- Stage " << setw(8) << right << stage << setw(15) << right << " -------------┐" << endl;
 	cout << "|" << setw(21) << left << " " << setw(22) << right << "|" << "          " << "|" << setw(21) << left << " " << setw(22) << right << "|" << endl;
 	//HP 출력
@@ -64,6 +64,7 @@ void Battle::playerAttack()
 {
 	cout << nowPlayer->getName() << "은/는 기본 공격을 시도했다!\n" << "입힌 피해량 : " << nowPlayer->getAttack() << endl;
 	nowMonster->setCurrentHP(nowMonster->getCurrentHP() - nowPlayer->getAttack());
+	Sleep(1000);
 }
 
 //플레이어 턴 : 스킬 사용
@@ -71,7 +72,6 @@ void Battle::playerAttack()
 void Battle::playerSkill() {
 	// 스킬 목록 가져오기
 	if (skill->GetSkills().empty()) {
-		std::cout << "사용 가능한 스킬이 없습니다!\n";
 		return;
 	}
 
@@ -107,105 +107,55 @@ void Battle::monsterAttack()
 {
 	cout << nowMonster->getName() << "의 기본공격이 " << nowPlayer->getName() << "에게 적중했다...\n" << "받은 피해량 : " << nowMonster->getAttack() << endl;
 	nowPlayer->setCurrHP(nowPlayer->getCurrHP() - nowMonster->getAttack());
+	Sleep(1000);
 }
 
 //-----------전투 진행 과정 함수 - Battle 클래스의 함수들을 자동으로 재생-------------
-void Battle::startBattle()
-{
-	//전투 1단계 / 다음 스테이지 정보 알림, Go or Back 선택
+void Battle::startBattle() {
 	bool isPlayerLive = true;
 	int stageChoose = 0;
-	while (stageChoose != 1 && stageChoose != 2)
-	{
+
+	while (stageChoose != 1 && stageChoose != 2) {
 		showStats();
-		cout << "다음 스테이지 : " << getStage()+1 << ". 도전하시겠습니까? (도전 : 1 / 이전 스테이지 재도전 : 2)" << endl;
+		cout << "다음 스테이지 : " << getStage() + 1 << ". 도전하시겠습니까? (도전 : 1 / 이전 스테이지 재도전 : 2)" << endl;
 		cin >> stageChoose;
 
-		if (stageChoose != 1 && stageChoose != 2)
-		{
+		if (stageChoose != 1 && stageChoose != 2) {
 			cout << "잘못된 숫자를 입력하셨습니다. 다시 입력해 주세요." << endl;
 		}
 	}
 
-	//전투 2단계 / 스테이지 설정
-	if (stageChoose == 1)
-	{
+	if (stageChoose == 1) {
 		setStage(getStage() + 1);
 		cout << "[ STAGE : " << getStage() << " ]" << " 용사님의 도전에 행운이 함께하길 바랍니다..." << endl;
 	}
-	else if (stageChoose == 2)
-	{
+	else if (stageChoose == 2) {
 		cout << "[ STAGE : " << getStage() << " ]" << " 도전을 위해서 수련을 택한 용사님을 응원합니다..." << endl;
 	}
 
-	//전투 3단계 / 전투 개시, 승리 또는 사망시 전투 종료
-	if (nowPlayer->getLevel() > getStage())
-	{
-		cout << "[ " << nowPlayer->getName() << " 선공 | " << nowMonster->getName() << " 후공 ] 플레이어의 레벨이 알 수 없는 힘에 저항했다!" << endl;
-		while (nowPlayer->getCurrHP() > 0 || nowMonster->getCurrentHP() > 0)
-		{
-			//플레이어 행동, 몬스터 처치시 전투 종료
-			Sleep(500);
-			playerBehavior();
-			if (nowMonster->getCurrentHP() < 0) 
-			{
-				isPlayerLive = true;
-				cout << "[ 승리 ] " << nowMonster->getName() << "을/를 처치했습니다. 승리를 축하합니다!" << endl;
-				break; 
-			}
-			//몬스터 행동, 플레이어 사망시 전투 종료
-			Sleep(500);
-			monsterAttack();
-			if (nowPlayer->getCurrHP() < 0)
-			{
-				isPlayerLive = false;
-				cout << "[ 패배 ] " << nowMonster->getName() << "의 강력한 일격에 " << nowPlayer->getName() << "(이)가 사망했습니다." << endl;
-				break;
-			}
-			setTurn(getTurn() + 1);
+	while (nowPlayer->getCurrHP() > 0 && nowMonster->getCurrentHP() > 0) {
+		showStats();
+		Sleep(500);
+		playerBehavior();
+		if (nowMonster->getCurrentHP() <= 0) {
+			isPlayerLive = true;
+			cout << "[ 승리 ] " << nowMonster->getName() << "을/를 처치했습니다. 승리를 축하합니다!" << endl;
+			break;
 		}
-	}
-	else
-	{
-		cout << "[ " << nowMonster->getName() << " 선공 | " << nowPlayer->getName() << " 후공 ] 스테이지의 알 수 없는 힘이 플레이어를 짓누른다!" << endl;
-		while (nowPlayer->getCurrHP() != 0 || nowMonster->getCurrentHP() != 0)
-		{
-			//몬스터 행동, 플레이어 사망시 전투 종료
-			Sleep(500);
-			monsterAttack();
-			if (nowPlayer->getCurrHP() < 0)
-			{
-				isPlayerLive = false;
-				cout << "[ 패배 ] " << nowMonster->getName() << "의 강력한 일격에 " << nowPlayer->getName() << "(이)가 쓰러졌습니다." << endl;
-				break;
-			}
-			//플레이어 행동, 몬스터 처치시 전투 종료
-			Sleep(500);
-			playerBehavior();
-			if (nowMonster->getCurrentHP() < 0)
-			{
-				isPlayerLive = true;
-				cout << "[ 승리 ] " << nowMonster->getName() << "을/를 처치했습니다. 승리를 축하합니다!" << endl;
-				break;
-			}
-			setTurn(getTurn() + 1);
+
+		Sleep(500);
+		monsterAttack();
+		if (nowPlayer->getCurrHP() <= 0) {
+			isPlayerLive = false;
+			cout << "[ 패배 ] " << nowMonster->getName() << "의 강력한 일격에 " << nowPlayer->getName() << "(이)가 사망했습니다." << endl;
+			break;
 		}
+		setTurn(getTurn() + 1);
 	}
 
-	//전투 4단계 / 승리시 보상 정산 및 진행 가능 여부 확인 / 사망시 인벤토리에 부활권 소유여부 검사 및 진행 가능 여부 확인
-	if (isPlayerLive == true)
-	{
-		canKeepGoing = true;
-		//보상 정산
-	}
-	else if (isPlayerLive == false)
-	{
-		canKeepGoing = false;
-		if (item->getConsumable().find("부활권") != item->getConsumable().end())
-		{
-			canKeepGoing = true;
-			item->itemUse("부활권");
-			cout << "눈이 점점 감기는 중에 인벤토리에서 부활권이 희미하게 빛나고 있습니다. 신비로운 힘이 " << nowPlayer->getName() << "을/를 감쌉니다." << endl;
-		}
+	if (!isPlayerLive && item->getConsumable().find("부활권") != item->getConsumable().end()) {
+		item->itemUse("부활권");
+		nowPlayer->setCurrHP(nowPlayer->getMaxHealth());
+		cout << "부활권으로 " << nowPlayer->getName() << "이/가 부활했습니다!" << endl;
 	}
 }
